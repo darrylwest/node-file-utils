82a39af914268f40f1e3c3fd9d37aa0cc000262c
darryl west <darryl.west@RainCitySoftware.com>
2014-07-20 16:54:32 -0700

    implemented tree walker example

--- examples/tree-walker.js
+++ examples/tree-walker.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 
-var TreeWalker = require('../lib/TreeWalker');
+var TreeWalker = require('../lib/TreeWalker'),
+    log = require('simple-node-logger').createSimpleLogger(),
+    walker = new TreeWalker({ log:log });
 
-console.log( 'not implemented yet...' );
+log.info('walk a known folder' );
+
+walker.walk( __dirname + '/../test', function(err, files) {
+    if (err) throw err;
+
+    files.forEach(function(file) {
+        log.info( file );
+    });
+
+    log.info( 'file list length: ', files.length );
+});
 
