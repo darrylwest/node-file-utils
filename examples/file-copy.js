3b29a1f2c050d4f72873750fb61c7e2857bf664f
darryl west <darryl.west@RainCitySoftware.com>
2014-07-20 16:44:02 -0700

    implemented file copy example

--- examples/file-copy.js
+++ examples/file-copy.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('not implemented yet...');
+var FileCopier = require('../lib/FileCopier'),
+    log = require('simple-node-logger').createSimpleLogger(),
+    copier = new FileCopier( { log:log } ),
+    src = __dirname + '/test-file.txt',
+    dest = __dirname + '/copied-file.txt';
+
+log.info('copy file...');
+
+copier.copy( src, dest, function(err) {
+    if (err) throw err;
+
+    log.info('file ', src, ' copied to ', dest);
+});
+
