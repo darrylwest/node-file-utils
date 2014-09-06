c13b23d9a60adf09a4ebb03986557c5105cb892b
darryl west <darryl.west@RainCitySoftware.com>
2014-09-06 14:25:09 -0700

    implemented static instace constructor method

--- examples/tree-walker.js
+++ examples/tree-walker.js
@@ -1,34 +1,33 @@
 #!/usr/bin/env node
 
 var TreeWalker = require('../lib/TreeWalker'),
-    log = require('simple-node-logger').createSimpleLogger(),
-    walker = new TreeWalker({ log:log });
+    walker = TreeWalker.createInstance();
 
 walker.onDirectory(function(path) {
-    log.info('new directory found: ', path);
+    console.log('new directory found: ', path);
 });
 
 walker.onFile(function(file) {
-    log.info('new file found: ', file);
+    console.log('new file found: ', file);
 });
 
 walker.walk( __dirname + '/../test', function(err, files) {
     if (err) throw err;
 
     files.forEach(function(file) {
-        log.info( file );
+        console.log( file );
     });
 
-    log.info( 'walk list length: ', files.length );
+    console.log( 'walk list length: ', files.length );
 });
 
 walker.find( __dirname + '/../test', /Mock/, function(err, files) {
     if (err) throw err;
 
     files.forEach(function(file) {
-        log.info( file );
+        console.log( file );
     });
 
-    log.info( 'find list length: ', files.length );
+    console.log( 'find list length: ', files.length );
 });
 
